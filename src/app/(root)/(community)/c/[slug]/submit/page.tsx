3f575e4ee9f126d7e67b7af0fccfaf5f61c6f0c1
Nathanael DCunha <nathanael.dcunha@gmail.com>
2024-03-04 22:14:00 +0530

    feat: added editor component

--- src/app/(root)/(community)/c/[slug]/submit/page.tsx
+++ src/app/(root)/(community)/c/[slug]/submit/page.tsx
@@ -1,9 +1,12 @@
+import { Editor } from '@/components/editor';
+import { commentExtensions } from '@/components/editor/extensions';
+
 export const metadata = {
-	title: "Submit a Post",
+	title: 'Submit a Post',
 };
 
 const Page = () => {
-	return <>Submit Post</>;
+	return <Editor extensions={commentExtensions} />;
 };
 
 export default Page;
