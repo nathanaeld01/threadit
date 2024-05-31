033e4b4d6c4eea818ed54cefe44586685834048c
Nathanael DCunha <nathanael.dcunha@gmail.com>
2024-05-31 13:42:52 +0530

    chore: added tailwind and config changes

--- apps/frontend/next.config.mjs
+++ apps/frontend/next.config.mjs
@@ -1,3 +1,11 @@
-const nextConfig = {};
+const nextConfig = {
+	eslint: { ignoreDuringBuilds: true },
+	transpilePackages: [
+		"@threadit/ui",
+		"@threadit/utils",
+		"@threadit/validators",
+	],
+	typescript: { ignoreBuildErrors: true },
+};
 
 export default nextConfig;
---
+++ apps/frontend/tailwind.config.js
@@ -0,0 +1,6 @@
+import baseTailwind from "@threadit/tailwind";
+
+export default {
+	content: ["./src/**/*.tsx", "./src/**/*.ts"],
+	presets: [baseTailwind],
+};
